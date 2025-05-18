4d37c5e1812ec88ceb08a5f33d3adbd2f8a6a4cb
nguyentrongbut <cloly031128@gmail.com>
2025-05-18 12:21:50 +0700

    init prj

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const helmet = require("helmet");
+const compression = require("compression");
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet())
+app.use(compression());
+
+// init db
+
+// init routes
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello Factipjs'
+    return res.status(200).json({
+        message: 'Hello World!',
+        metadata: strCompress.repeat(10000)
+    })
+})
+
+// handling error
+
+module.exports = app

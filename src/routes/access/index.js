37e6a9e99cf3a510536bcf4dbdd169ec6e9c4059
nguyentrongbut <cloly031128@gmail.com>
2025-05-19 13:17:43 +0700

    feat: handling errorhandler in api

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -3,8 +3,10 @@
 const express = require('express')
 const router = express.Router()
 const accessController = require('../../controllers/access.controller')
+const {asyncHandler} = require("../../auth/checkAuth");
+
 
 // signUp
-router.post('/shop/signup', accessController.signUp)
+router.post('/shop/signup', asyncHandler(accessController.signUp))
 
 module.exports = router
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -8,6 +8,7 @@ const crypto = require('node:crypto')
 const KeyTokenService = require('./keyToken.service')
 const {createTokenPair} = require("../auth/authUtils");
 const {getInfoData} = require("../utils");
+const {BadRequestError} = require("../core/error.response");
 
 const RoleShop = {
     SHOP: 'SHOP',
@@ -19,76 +20,64 @@ const RoleShop = {
 class AccessService {
 
     static signUp = async ({name, email, password}) => {
-        try {
-            // step1: check email exists??
 
-            const holderShop = await shopModel.findOne({email}).lean()
+        // step1: check email exists??
 
-            if (holderShop) {
-                return {
-                    code: 'xxxx',
-                    message: 'Shop already registered',
-                }
-            }
+        const holderShop = await shopModel.findOne({email}).lean()
 
-            const passwordHash = await bcrypt.hash(password, 10)
+        console.log("holderShop", holderShop)
+        if (holderShop) {
+            throw new BadRequestError('Error: Shop already registered!')
+        }
 
-            const newShop = await shopModel.create({
-                name, email, password: passwordHash, roles: [RoleShop.SHOP]
+        const passwordHash = await bcrypt.hash(password, 10)
+
+        const newShop = await shopModel.create({
+            name, email, password: passwordHash, roles: [RoleShop.SHOP]
+        })
+
+        if (newShop) {
+            // create privateKey, publicKey
+            const privateKey = crypto.randomBytes(64).toString('hex')
+            const publicKey = crypto.randomBytes(64).toString('hex')
+
+            console.log({privateKey, publicKey}) // save collection KeyStore
+
+            const keyStore = await KeyTokenService.createKeyToken({
+                userId: newShop._id,
+                publicKey,
+                privateKey
             })
 
-            if (newShop) {
-                // create privateKey, publicKey
-                const privateKey = crypto.randomBytes(64).toString('hex')
-                const publicKey = crypto.randomBytes(64).toString('hex')
+            if (!keyStore) {
+                throw new BadRequestError('Error: Create KeyStore failed!')
+            }
 
-                console.log({privateKey, publicKey}) // save collection KeyStore
-
-                const keyStore = await KeyTokenService.createKeyToken({
-                    userId: newShop._id,
-                    publicKey,
-                    privateKey
-                })
-
-                if (!keyStore) {
-                    return {
-                        code: 'xxxx',
-                        message: 'publicKeyString error'
-                    }
-                }
-
-                // created token pair
-                const tokens = await createTokenPair({
+            // created token pair
+            const tokens = await createTokenPair({
                     userId: newShop._id,
                     email
                 },
-                    publicKey,
-                    privateKey
-                    )
+                publicKey,
+                privateKey
+            )
 
-                console.log(`Created Token Success::`, tokens)
+            console.log(`Created Token Success::`, tokens)
 
-                return {
-                    code: 201,
-                    metadata: {
-                        shop: getInfoData({
-                            fileds: ['_id', 'name', 'email'], object: newShop
-                        }),
-                        tokens
-                    }
+            return {
+                code: 201,
+                metadata: {
+                    shop: getInfoData({
+                        fileds: ['_id', 'name', 'email'], object: newShop
+                    }),
+                    tokens
                 }
             }
+        }
 
-            return {
-                code: 200,
-                metadata: null
-            }
-        } catch (error) {
-            return {
-                code: 'xxx',
-                message: error.message,
-                status: 'error'
-            }
+        return {
+            code: 200,
+            metadata: null
         }
     }
 }
